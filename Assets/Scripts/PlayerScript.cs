94d6d7a96475c2ae3d54b36b3960bdc1afb469cb
unknown <sara_peterson@knights.ucf.edu>
2020-10-26 00:14:09 -0400

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+    public float speed;
+    public Text score;
+    public Text livesText;
+    public Text loseText;
+    public Text winText;
+    private int scoreValue = 0;
+    private int lives;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    private bool facingRight = true;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = "Score: " + scoreValue.ToString();
+        lives = 3;
+        livesText.text = "Lives: " + lives.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        anim = GetComponent<Animator>();
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement, verMovement));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector2(30.0f, -3.0f);
+                lives = 3;
+                SetLivesText();
+            }
+        }
+
+        else if (collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            livesText.text = "Lives: " + lives.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if (scoreValue >=8)
+        {
+            winText.text = "You Win! Game created by Sara Peterson";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        if (lives <=0)
+        {
+            loseText.text = "You Lose! Game created by Sara Peterson";
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives<=0)
+        {
+            loseText.text = "You Lose! Game created by Sara Peterson";
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+}
